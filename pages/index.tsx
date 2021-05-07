52392f8a5aabdda941dcda3f9af4d69f5ffa7967
Stephen Dickinson <stephencottontail@me.com>
2021-05-07 17:37:21 -0600

    POC for getting front matter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,52 @@
-export default function HomePage(): JSX.Element {
-	return <h1>Home Page!</h1>;
-}
+import fs from "fs";
+import { join } from "path";
+import { GetStaticProps } from "next";
+
+import remark from "remark";
+import frontmatter from "remark-frontmatter";
+import extract from "remark-extract-frontmatter";
+import yaml from "yaml";
+
+const hasKeys = <T extends object, K extends PropertyKey>(
+	obj: T,
+	keys: K | K[]
+): obj is T & { [P in K]: unknown } => {
+	return Array.isArray(keys) ? keys.every((k) => k in obj) : keys in obj;
+};
+
+const HomePage = (props: Record<string, string[]>): JSX.Element => {
+	const { cats } = props;
+
+	return (
+		<div>
+			{cats.map((cat, index) => (
+				<p key={index}>{cat}</p>
+			))}
+		</div>
+	);
+};
+
+export const getStaticProps: GetStaticProps = async () => {
+	const cats: string[] = [];
+	const path = join(process.cwd(), "posts");
+	const posts = fs.readdirSync(path);
+
+	posts.forEach(async (post) => {
+		const contents = fs.readFileSync(join(path, post));
+		const results = await remark()
+			.use(frontmatter, ["yaml"])
+			.use(extract, { yaml: yaml.parse })
+			.process(contents);
+
+		if (typeof results.data === "object" &&
+			hasKeys(results.data, "category")) {
+			cats.push(results.data.category as string);
+		}
+	});
+
+	return {
+		props: { cats },
+	};
+};
+
+export default HomePage;
---
+++ posts/Ulysses.md
@@ -0,0 +1,11 @@
+---
+title: Ulysses
+date: May 5, 2021 at 7:51 AM
+category: testing
+---
+
+# Ulysses
+I created this note with another Siri Shortcut and edited it in Ulysses. I like Ulysses in this particular case because it can use the iPadOS edit-in-place feature so I don’t have to manually export to the `posts` folder whenever I edit a note.
+
+## Typography
+Actually, I don’t really need to test typography features again. So I won’t.

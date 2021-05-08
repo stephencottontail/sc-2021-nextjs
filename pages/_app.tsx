3196d06bca60aa830064ad88dd3a346533fc1201
Stephen Dickinson <stephencottontail@me.com>
2021-05-08 10:47:46 -0600

    Set up `twin.macro`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,16 @@
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { Fragment } from "react";
+import { GlobalStyles } from "twin.macro";
 
 import "../styles/styles.css";
 
-function MyApp({ Component, pageProps }: AppProps ): JSX.Element {
-	return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return (
+		<Fragment>
+			<GlobalStyles />
+			<Component {...pageProps} />
+		</Fragment>
+	);
 }
 
 export default MyApp;

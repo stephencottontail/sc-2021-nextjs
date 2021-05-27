cefaa306e6a6cd88d9c875c411060e242dc56945
Stephen Dickinson <stephencottontail@me.com>
2021-05-26 20:17:43 -0600

    Single post view

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -1,49 +1,70 @@
-import tw, { styled } from "twin.macro";
+import tw from "twin.macro";
 
-const Prose = styled.article({
-	...tw`p-normal bg-gray-100 text-gray-700`,
-	backgroundImage: "linear-gradient(green 1px, transparent 1px)",
-	backgroundSize: "1px 1.5rem",
-	h1: tw`mb-normal font-extrabold text-4xl`,
-	h2: tw`mb-normal font-bold text-2xl`,
-	h3: tw`mb-normal font-bold text-xl`,
-	p: tw`mb-normal`,
-	a: tw`font-bold text-blue-700 border-blue-700 border-b-2 focus:border-b-4 hover:border-b-4 active:border-b-4 motion-safe:transition-all`,
-	ul: {
-		...tw`mb-normal pl-normal list-disc list-inside`,
-		ul: {
-			...tw`m-none`,
-		},
-		ol: {
-			...tw`m-none`,
-		},
-	},
-	ol: {
-		...tw`mb-normal pl-normal list-decimal list-inside`,
-		ul: {
-			...tw`m-none`,
-		},
-		ol: {
-			...tw`m-none`,
-		},
-	},
-	code: {
-		...tw`bg-gray-700 text-gray-100`,
-		padding: "2px",
-		borderRadius: "4px",
-	},
-	blockquote: {
-		...tw`space-y-normal mb-normal p-normal bg-blue-100`,
-		p: {
-			...tw`m-none`,
-		},
-	},
-	pre: {
-		...tw`mb-normal p-normal overflow-x-scroll`,
-		code: {
-			...tw`p-none bg-gray-100 text-gray-700`,
-		},
-	},
-});
+import { createElement, Fragment } from "react";
+import remark from "remark";
+import frontmatter from "remark-frontmatter";
+import extract from "remark-extract-frontmatter";
+import yaml from "yaml";
+import toRehype from "remark-rehype";
+import toReact from "rehype-react";
+
+const Prose = ({ markdown }: { markdown: string }): JSX.Element => {
+	const contents = remark()
+		.use(frontmatter, [{ type: "yaml", anywhere: true, marker: "-" }])
+		.use(extract, { yaml: yaml.parse })
+		.use(toRehype)
+		.use(toReact, { createElement, Fragment })
+		.processSync(markdown);
+
+	return (
+		<article
+			css={{
+				...tw`p-normal bg-gray-100 text-gray-700`,
+				h1: tw`mb-normal font-extrabold text-4xl`,
+				h2: tw`mb-normal font-bold text-2xl`,
+				h3: tw`mb-normal font-bold text-xl`,
+				p: tw`mb-normal`,
+				a: tw`font-bold text-blue-700 border-blue-700 border-b-2 focus:border-b-4 hover:border-b-4 active:border-b-4 motion-safe:transition-all`,
+				ul: {
+					...tw`mb-normal pl-normal list-disc list-inside`,
+					ul: {
+						...tw`m-none`,
+					},
+					ol: {
+						...tw`m-none`,
+					},
+				},
+				ol: {
+					...tw`mb-normal pl-normal list-decimal list-inside`,
+					ul: {
+						...tw`m-none`,
+					},
+					ol: {
+						...tw`m-none`,
+					},
+				},
+				code: {
+					...tw`bg-gray-700 text-gray-100`,
+					padding: "2px",
+					borderRadius: "4px",
+				},
+				blockquote: {
+					...tw`space-y-normal mb-normal p-normal bg-blue-100`,
+					p: {
+						...tw`m-none`,
+					},
+				},
+				pre: {
+					...tw`mb-normal p-normal overflow-x-scroll`,
+					code: {
+						...tw`p-none bg-gray-100 text-gray-700`,
+					},
+				},
+			}}
+		>
+			{contents.result}
+		</article>
+	);
+};
 
 export default Prose;
--- pages/[category]/[slug].tsx
+++ pages/[category]/[slug].tsx
@@ -1,18 +1,36 @@
+import fs from "fs";
+import { join } from "path";
+
+import { Fragment } from "react";
+import Prose from "../../components/Prose";
+
 import { GetStaticPaths, GetStaticProps, GetStaticPropsContext } from "next";
 import { matchSlugsWithCategories } from "../../utils/utils";
 
-const Slug = (props: Record<string, string>): JSX.Element | null => {
-	const { slug } = props;
+// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
+const Slug = (props: Record<string, string>) => {
+	const { slug, markdown } = props;
 
-	return <h1>{slug.toUpperCase()}</h1>;
+	return (
+		<Fragment>
+			<h1>{slug.toUpperCase()}</h1>
+			<Prose markdown={markdown} />
+		</Fragment>
+	);
 };
 
 export const getStaticProps: GetStaticProps = async (
 	context: GetStaticPropsContext
 ) => {
+	const { slug } = context.params;
+	const markdown = fs.readFileSync(
+		join(process.cwd(), "posts", slug + ".md")
+	);
+
 	return {
 		props: {
-			slug: context.params.slug,
+			slug,
+			markdown: markdown.toString(),
 		},
 	};
 };

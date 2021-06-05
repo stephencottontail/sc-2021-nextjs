5f06aaf0d9cf76585e6a8c05386c761afd67b603
Stephen Dickinson <stephencottontail@me.com>
2021-06-05 17:52:07 -0600

    Use PNG for favicon and social sharing

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,6 +8,12 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 		<Fragment>
 			<Head>
 				<title>Stephen</title>
+				<link
+					rel="icon"
+					type="image/png"
+					sizes="512x512"
+					href="/images/sc-2021-nextjs.png"
+				/>
 				<meta
 					name="description"
 					content="I'm Stephen, and I'm not a real web developer. Or am I? It's hard to know anymore."
@@ -21,7 +27,7 @@ function MyApp({ Component, pageProps }: AppProps): JSX.Element {
 				/>
 				<meta
 					property="og:image"
-					content="https://stephencottontail.com/images/sc-2021-nextjs.svg"
+					content="https://stephencottontail.com/images/sc-2021-nextjs.png"
 				/>
 				<meta property="og:image:height" content="512" />
 				<meta property="og:image:width" content="512" />
